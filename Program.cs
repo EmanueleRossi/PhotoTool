99fc65bc666153d35ded10f5eba240dea5821fde
ROSSI, Emanuele <emanuele.rossi@gmail.com>
2020-04-23 20:54:53 +0200

    Initial Import!

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace PhotoTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] files = Directory.GetFiles(".");
+            foreach (string file in files) 
+            {
+                Console.WriteLine($"Processing file {file}");
+            }            
+        }
+    }
+}
