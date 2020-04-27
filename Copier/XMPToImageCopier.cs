afa4d8080141264b5f5055dc3752aa471062d200
ROSSI, Emanuele <emanuele.rossi@gmail.com>
2020-04-27 09:17:39 +0200

    - Now file/path specification works; - Fixed file format output!

--- Copier/XMPToImageCopier.cs
+++ Copier/XMPToImageCopier.cs
@@ -1,15 +1,17 @@
 using System;
 using System.Diagnostics;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 using XmpCore;
 
 namespace PhotoTool.Copier
 {
     class XMPToImageCopier
     {
-        public static readonly string[] supportedExtensions = { ".gif", ".heic", ".jpg", ".jpeg", ".png", ".mp4" };
+        public static readonly string[] supportedExtensions = { ".jpg", ".jpeg", ".mp4" };
 
         private FileInfo XmpFile;
 
@@ -51,7 +53,6 @@ namespace PhotoTool.Copier
                     else
                     {
                         Program.MainLogger.Information($"DateTimeOriginal TAG in file {correspondingImageFile} was already set to {DateTimeOriginal}");                      
-                        //FIXME DON'T RENAME XMP!
                     }
                 }
                 else
@@ -79,7 +80,9 @@ namespace PhotoTool.Copier
 
             if (!string.IsNullOrEmpty(exifToolOutput)) 
             {
-                ExifToolResponse jsonExifToolOutput = JsonSerializer.Deserialize<ExifToolResponse[]>(exifToolOutput).First();                
+                JsonSerializerOptions serializerOptions = new JsonSerializerOptions();
+                serializerOptions.Converters.Add(new DateTimeConverterUsingDateTimeParseAsFallback());
+                ExifToolResponse jsonExifToolOutput = JsonSerializer.Deserialize<ExifToolResponse[]>(exifToolOutput, serializerOptions).First();                
                 return jsonExifToolOutput.DateTimeOriginal;
             }
             else
@@ -94,5 +97,24 @@ namespace PhotoTool.Copier
             string imageFileExtension = supportedExtensions.Where(e => File.Exists(Path.ChangeExtension(XmpFile.FullName, e))).Single();
             return new FileInfo(Path.ChangeExtension(XmpFile.FullName, imageFileExtension));
         }        
+
+        public class DateTimeConverterUsingDateTimeParseAsFallback : JsonConverter<DateTime>
+        {
+            private const string customFormat = "yyyy:MM:dd HH:mm:ss"; 
+            public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+            {
+                if (!reader.TryGetDateTime(out DateTime value))
+                {
+                    Program.MainLogger.Verbose($"Trying to parse {reader.GetString()}"); 
+                    value = DateTime.ParseExact(reader.GetString(), customFormat, CultureInfo.InvariantCulture);
+                }
+                return value;
+            }
+
+            public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options)
+            {
+                writer.WriteStringValue(value.ToString(customFormat));
+            }
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -19,39 +19,24 @@ namespace PhotoTool
             try
             { 
                 if (args?.Length > 0)
-                {                    
-                    string[] files = Directory.GetFiles(args[0]);
+                {            
+                    MainLogger.Information($"Processing fil");        
+                    string[] files = Directory.GetFiles(args[0], args[1] ?? "*.*", SearchOption.TopDirectoryOnly);
                     foreach (string file in files) 
                     {                        
                         MainLogger.Verbose(Environment.NewLine);                                                              
-                        MainLogger.Verbose($"Analyzing file {file}");                                                              
-                        if (args?.Length > 1)
+                        MainLogger.Information($"Processing file {file}...");                                                                                                
+                        if (string.Equals(Path.GetExtension(file), ".xmp", StringComparison.OrdinalIgnoreCase))
                         {
-                            // FIXME
-                            if(file.Contains(args[1]))
-                            {         
-                                MainLogger.Information($"Processing...");                                                                                                
-                                if (string.Equals(Path.GetExtension(file), ".xmp", StringComparison.OrdinalIgnoreCase))
-                                {
-                                    FileInfo xmpFileInfo = new FileInfo(file);
-                                    XMPToImageCopier copier = new XMPToImageCopier(xmpFileInfo);
-                                    copier.copyDateCreatedToDateTimeOriginal();      
-                                    File.Move(file, Path.ChangeExtension(xmpFileInfo.FullName, ".xmp.done"), true);
-                                }
-                                else
-                                {
-                                    MainLogger.Information($"Not an XMP file!");                
-                                }     
-                            } 
-                            else
-                            {
-                                MainLogger.Verbose($"File {file} does not matches pattern {args[1]}.");   
-                            }                                                         
+                            FileInfo xmpFileInfo = new FileInfo(file);
+                            XMPToImageCopier copier = new XMPToImageCopier(xmpFileInfo);
+                            copier.copyDateCreatedToDateTimeOriginal();      
+                            File.Move(file, Path.ChangeExtension(xmpFileInfo.FullName, ".xmp.done"), true);
                         }
                         else
                         {
-                            MainLogger.Error($"Parameter #2 is needed!");                   
-                        }
+                            MainLogger.Information($"Not an XMP file!");                
+                        }                                                            
                     }  
                 }
                 else
@@ -67,12 +52,6 @@ namespace PhotoTool
             {
                 MainLogger.Dispose();
             }            
-        }
-
-        private static bool FitsMask(string fileName, string fileMask)
-        {
-            Regex mask = new Regex('^' + fileMask.Replace(".", "[.]").Replace("*", ".*").Replace("?", ".") + '$', RegexOptions.IgnoreCase);
-            return mask.IsMatch(fileName);
-        }   
+        }  
     }
 }
